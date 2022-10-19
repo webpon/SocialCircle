5f074890b5418da7539080178528b49ba1bb814a
lwj <210579781@qq.com>
2022-10-19 20:21:51 +0800

    集成mq+即时通讯

---
+++ server/managerServer/src/main/java/com/socialCircle/ManagerServerApplication.java
@@ -0,0 +1,11 @@
+package com.socialCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerServerApplication.class,args);
+    }
+}
