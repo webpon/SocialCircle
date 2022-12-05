b11eae2c38219d48f31a51976f93c68a461e9605
lwj <210579781@qq.com>
2022-12-05 13:57:09 +0800

    更改图片接口

--- server/userServer/src/main/java/com/socialCircle/controller/CaptchaController.java
+++ server/userServer/src/main/java/com/socialCircle/controller/CaptchaController.java
@@ -6,10 +6,10 @@ import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
 import com.socialCircle.common.RedisUtil;
 import com.socialCircle.constant.RedisKey;
+import com.socialCircle.entity.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -37,35 +37,24 @@ public class CaptchaController {
     private RedisUtil redisUtil;
 
     @GetMapping
-    public ModelAndView getKaptchaImage(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        HttpSession session = request.getSession();
-
-        response.setDateHeader("Expires", 0);
-
-        // Set standard HTTP/1.1 no-cache headers.
-        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
-
-        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
-        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-
-        // Set standard HTTP/1.0 no-cache header.
-        response.setHeader("Pragma", "no-cache");
-
-        // return a jpeg
-        response.setContentType("image/jpeg");
-
-        // create the text for the image
+    @ResponseBody
+    public Result getKaptchaImagKey(){
         String capText = captchaProducer.createText();
         String key = UUID.randomUUID().toString().substring(15);
         redisUtil.save(RedisKey.IMAGE_CODE + key, capText, 10, TimeUnit.MINUTES);
-        response.setHeader("Code-Key", key);
-        // store the text in the session
-        session.setAttribute(Constants.KAPTCHA_SESSION_KEY, capText);
-
+        return Result.ok("ok", key);
+    }
+    @GetMapping("/image")
+    public ModelAndView getKaptchaImage(@RequestParam String key, HttpServletResponse response) throws Exception {
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        response.setHeader("Pragma", "no-cache");
+        response.setContentType("image/jpeg");
+        String bean = redisUtil.getBean(key, String.class);
         // create the image with the text
-        BufferedImage bi = captchaProducer.createImage(capText);
+        BufferedImage bi = captchaProducer.createImage(bean);
         ServletOutputStream out = response.getOutputStream();
-
         // write the data out
         ImageIO.write(bi, "jpg", out);
         try {
