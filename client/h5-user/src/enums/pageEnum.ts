0bfaf6032c5b32317b7504482ca62aa03d90a905
webpon <2249096563@qq.com>
2022-12-16 02:03:16 +0800

    更新

--- client/h5-user/src/enums/pageEnum.ts
+++ client/h5-user/src/enums/pageEnum.ts
@@ -3,8 +3,8 @@ export enum PageEnum {
   BASE_LOGIN = '/login',
   BASE_LOGIN_NAME = 'Login',
   // 首页
-  BASE_HOME = '/dashboard',
-  BASE_HOME_REDIRECT = '/dashboard',
+  BASE_HOME = '/home',
+  BASE_HOME_REDIRECT = '/home',
   // 错误
   ERROR_PAGE_NAME = 'ErrorPage',
 }
