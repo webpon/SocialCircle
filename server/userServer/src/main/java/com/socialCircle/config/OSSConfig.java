dcae093801bdce8d83e5f6d697d7fbca45f66052
lwj <210579781@qq.com>
2022-12-15 15:49:24 +0800

    OSS

---
+++ server/userServer/src/main/java/com/socialCircle/config/OSSConfig.java
@@ -0,0 +1,26 @@
+package com.socialCircle.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OSSConfig {
+
+    @Value("${alicloud.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${alicloud.access-id}")
+    private String accessKey;
+
+    @Value("${alicloud.secret-key}")
+    private String accessId;
+
+    @Bean
+    public OSS getClient(){
+        return new OSSClientBuilder().build(endpoint, accessId, accessKey);
+    }
+}
---
+++ server/userServer/src/main/java/com/socialCircle/controller/OssController.java
@@ -0,0 +1,70 @@
+package com.socialCircle.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.socialCircle.entity.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class OssController {
+
+    @Resource
+    private OSS ossClient;
+
+    @Value("${alicloud.oss.bucket}")
+    private String bucket;
+
+    @Value("${alicloud.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${alicloud.access-id}")
+    private String accessKey;
+
+    @Value("${alicloud.secret-key}")
+    private String accessId;
+
+    @RequestMapping("/oss")
+    public Result policy(){
+        String host = "https://"+bucket+"."+endpoint;
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format+"/";
+        Map<String, String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes(StandardCharsets.UTF_8);
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessId", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return Result.ok(respMap);
+    }
+}
