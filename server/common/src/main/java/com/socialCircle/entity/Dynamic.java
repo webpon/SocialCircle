5201f82c295dff03e63779bb3942b8bf5d3a25ec
lwj <210579781@qq.com>
2022-12-20 16:25:07 +0800

    删除动态标题

--- server/common/src/main/java/com/socialCircle/entity/Dynamic.java
+++ server/common/src/main/java/com/socialCircle/entity/Dynamic.java
@@ -20,11 +20,6 @@ public class Dynamic implements Serializable {
     private Integer id;
 
     /**
-     * 标题
-     */
-    private String title;
-
-    /**
      * 内容
      */
     private String content;
